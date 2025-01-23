25cc0dec31a54e579a24277a021412904fd58e3e
Kyle-KAO <kyle.kao@aimestechcp.com>
2025-01-23 11:38:57 +0800

    publicPath

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/kyle-kao.github.io/',
       // analyze: true,
       env: {
         API: 'https://dahua.metcfire.com.tw/',
