6c52f056453c4592389fedce8a8f426f47dafda0
Kyle-KAO <kyle.kao@aimestechcp.com>
2025-01-23 11:15:57 +0800

    interviewKao

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,7 +4,13 @@
       <div class="q-mb-xl">
         <q-input v-model="tempData.name" label="姓名" />
         <q-input v-model="tempData.age" label="年齡" />
-        <q-btn color="primary" class="q-mt-md">新增</q-btn>
+        <q-btn color="primary" class="q-mt-md" @click="handleClickOption({
+          label: '新增',
+          icon: 'add',
+          status: 'add',
+          }, tempData)"
+          >新增
+        </q-btn>
       </div>
 
       <q-table
@@ -79,13 +85,26 @@
 
 <script setup lang="ts">
 import axios from 'axios';
+import { useQuasar } from 'quasar'
 import { QTableProps } from 'quasar';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { useCounterStore } from 'stores/example-store'
+
 interface btnType {
   label: string;
   icon: string;
   status: string;
 }
+
+interface rowType {
+  ID?: string;
+  name?: string;
+  age?: number | string;
+}
+
+const userStore = useCounterStore()
+const $q = useQuasar()
+
 const blockData = ref([
   {
     name: 'test',
@@ -119,12 +138,79 @@ const tableButtons = ref([
   },
 ]);
 
-const tempData = ref({
+const tempData = ref<rowType>({
   name: '',
   age: '',
 });
-function handleClickOption(btn, data) {
-  // ...
+
+onMounted(async() => {
+  try {
+    await getList()
+  } catch (err) {
+    console.log('err:: ', err)
+  }
+})
+
+const getList = async() => {
+  try {
+    await userStore.getList()
+    blockData.value = userStore.userList
+  } catch (err) {
+    console.log('err::', err)
+  }
+}
+
+const handleClickOption = async(btn: btnType, data: rowType) => {
+  try {
+    console.log('btn:: ', btn)
+    console.log('data:: ', data)
+    switch(btn.status) {
+      case 'add':
+        const age = tempData.value.age
+        if(!tempData.value.name || !age) {
+          alert('姓名或年齡不能空白')
+          return 
+        }
+        if(isNaN(+age) || +age <= 0 || !Number.isInteger(+age)) {
+          alert('不得空白&限輸入數字(正整數) ')
+          return
+        }
+        await userStore.addList(tempData.value)
+        await getList()
+        break
+      case 'edit':
+        $q.dialog({
+          title: '提示',
+          message: '是否確定修改此資料?',
+          cancel: true,
+          persistent: true
+        }).onOk(async() => {
+          await axios.patch('https://dahua.metcfire.com.tw/api/CRUDTest', data)
+          await getList()
+        }).onCancel(() => {
+          console.log('>>>> Cancel')
+        })
+        break
+      case 'delete':
+        const { id } = data as { id: string }
+        $q.dialog({
+          title: '提示',
+          message: '是否確定刪除此資料?',
+          cancel: true,
+          persistent: true
+        }).onOk(async() => {
+          await axios.delete(`https://dahua.metcfire.com.tw/api/CRUDTest/${id}`)
+          await getList()
+        }).onCancel(() => {
+          console.log('>>>> Cancel')
+        })
+        break
+      default:
+        break
+    }
+  } catch (err) {
+    console.log('err:: ', err)
+  }
 }
 </script>
 
--- src/stores/example-store.ts
+++ src/stores/example-store.ts
@@ -1,8 +1,16 @@
 import { defineStore } from 'pinia';
+import { api } from '../boot/axios'
+
+interface rowType {
+  ID?: string;
+  name?: string;
+  age?: number | string;
+}
 
 export const useCounterStore = defineStore('counter', {
   state: () => ({
     counter: 0,
+    userList: [],
   }),
   getters: {
     doubleCount: (state) => state.counter * 2,
@@ -11,5 +19,20 @@ export const useCounterStore = defineStore('counter', {
     increment() {
       this.counter++;
     },
+    async getList() {
+      try {
+        const res = await api.get('/api/CRUDTest/a')
+        this.userList = res.data
+      } catch(err) {
+        console.log('err::', err)
+      }
+    },
+    async addList(params: rowType) {
+      try {
+        await api.post('/api/CRUDTest', params)
+      } catch(err) {
+        console.log('err::', err)
+      }
+    }
   },
 });
