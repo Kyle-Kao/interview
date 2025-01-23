b2e18aab378a43f26dcd150076d626017bf9f519
Kyle-KAO <kyle.kao@aimestechcp.com>
2025-01-23 11:51:38 +0800

    build

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: process.env.NODE_ENV === 'production' ? '/spa/' : '/',
+      publicPath: '/spa/',
       // analyze: true,
       env: {
         API: 'https://dahua.metcfire.com.tw/',
